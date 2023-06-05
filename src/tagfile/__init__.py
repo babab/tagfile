c16ca61c4b2732d26ce99a989ef51a72200cac93
Benjamin Althues <benjamin@babab.nl>
2023-06-05 22:18:45 +0200

    Add __main__ module to support `python -m tagfile`

--- src/tagfile/__init__.py
+++ src/tagfile/__init__.py
@@ -39,7 +39,7 @@ import yaml
 
 __author__ = "Benjamin Althues"
 __copyright__ = "Copyright (C) 2015-2023  Benjamin Althues"
-__version__ = '0.2.0a0'
+__version__ = '0.2.0a1'
 versionStr = 'tagfile {}'.format(__version__)
 
 
