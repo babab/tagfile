94d9cb595d77980bfc35dbd3a0859f96f9870a15
Benjamin Althues <benjamin@babab.nl>
2023-10-21 12:15:36 +0200

    Add generic class for validating config settings

--- src/tagfile/common.py
+++ src/tagfile/common.py
@@ -41,6 +41,79 @@ class ProgrammingError(Exception):
     pass
 
 
+class ConfigValidator:
+    def __init__(self, top_dict):
+        self.top_dict = top_dict
+
+    def parse_dots(self, name):
+        names = name.split('.')
+        if len(names) == 1:
+            struct = self.top_dict
+        elif len(names) == 2:
+            struct = self.top_dict[names[0]]
+        elif len(names) == 3:
+            struct = self.top_dict[names[0]][names[1]]
+        elif len(names) == 4:
+            struct = self.top_dict[names[0]][names[1]][names[2]]
+        elif len(names) == 5:
+            struct = self.top_dict[names[0]][names[1]][names[2]][names[3]]
+        else:
+            raise ProgrammingError('config structure is too nested')
+        realname = names[-1]
+        return (struct, realname)
+
+    def is_bool(self, name):
+        parent, realname = self.parse_dots(name)
+        if realname not in parent:
+            raise ConfigError(f'Missing "{name}" setting')
+        if not type(parent[realname]) is bool:
+            raise ConfigError(f'Value for "{name}" must be a bool')
+
+    def is_list(self, name):
+        parent, realname = self.parse_dots(name)
+        if realname not in parent:
+            raise ConfigError(f'Missing "{name}" setting')
+        if not type(parent[realname]) is list:
+            raise ConfigError(f'Value for "{name}" must be a list')
+
+    def is_dict(self, name, min_size=None):
+        parent, realname = self.parse_dots(name)
+        if realname not in parent:
+            raise ConfigError(f'Missing "{name}" section')
+        if not type(parent[realname]) is dict:
+            raise ConfigError(f'{name} is not a dictionary')
+        if min_size:
+            if len(parent[realname]) < min_size:
+                raise ConfigError(
+                    f'{name} has less than {min_size} '
+                    f'item{"s" if len(min_size) > 1 else ""}'
+                )
+
+    def is_str(self, name, options=[]):
+        '''Will validate if value is one of the options when not empty.'''
+        parent, realname = self.parse_dots(name)
+        if realname not in parent:
+            raise ConfigError(f'Missing "{name}" setting')
+        if not type(parent[realname]) is str:
+            raise ConfigError('Value for "{name}" must be string')
+        if options and parent[realname] not in options:
+            raise ConfigError(
+                f'Setting "{name}" is not valid.\n'
+                f'Valid options are: {",".join(options)}'
+            )
+
+    def is_int(self, name, vmin=None, vmax=None):
+        parent, realname = self.parse_dots(name)
+        if realname not in parent:
+            raise ConfigError(f'Missing "{name}" setting')
+        if not type(parent[realname]) is int:
+            raise ConfigError(f'Value for "{name}" must be a list')
+        if vmin is not None and parent[realname] < vmin:
+            raise ConfigError(f'Value for "{name}" must be >= {vmin}')
+        if vmax is not None and parent[realname] > vmax:
+            raise ConfigError(f'Value for "{name}" must be <= {vmax}')
+
+
 def invertexpanduser(path):
     '''Replaces a substring of `os.path.expanduser(path)` with ``~``.'''
     home_abspath = os.path.expanduser('~')
